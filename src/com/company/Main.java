9b3cb5175ef322e27fb36d5020e777f9a3f69cf9
Luis Alejandro <alfaroalejandro597@gmail.com>
2021-03-18 21:27:04 -0600

    Creacion de la clase patient

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-
+        //Patient patientOne = new Patient("Luis Alejandro", "alfaroalejandro597@gmail.com");
         UIMenu.showMenu();
-
     }
 }
--- src/com/company/Patient.java
+++ src/com/company/Patient.java
@@ -1,4 +1,17 @@
 package com.company;
 
 public class Patient {
+    String name, email, address, phoneNumber, birthday, blood;
+    Double weight,height;
+
+    // constructor
+    Patient(String name, String email) {
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        this.birthday = birthday;
+        this.blood = blood;
+    }
+
 }
